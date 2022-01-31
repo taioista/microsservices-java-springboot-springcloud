bfbd564a28d129d3433acd1ba47e71193b0008af
ppires <pe8542@gmail.com>
2022-01-31 11:43:40 -0300

    hr-worker configured with postgres in dev profile

--- hr-worker/src/main/java/com/dev/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/dev/hrworker/resources/WorkerResource.java
@@ -8,7 +8,6 @@ import com.dev.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
